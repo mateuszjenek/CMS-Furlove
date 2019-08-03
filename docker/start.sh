6aa13563739048226ef9798c9720d0ae965ef83b
Mateusz Jenek <mateusz.jenek@gmail.com>
2019-08-03 23:35:37 +0200

    Fixed error HTTP Timeout

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-{cd ../src && npm install && composer install && yarn encore dev}
-docker-compose up
+( cd ../src && npm install && composer install && yarn encore dev )
+COMPOSE_HTTP_TIMEOUT=100000 docker-compose up
